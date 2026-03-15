94e9ba865abf8e6ff5a489cc7579c33a12d801c7
Grant Cox <42614867+gcox32@users.noreply.github.com>
2026-03-14 23:13:34 -0400

    Fix player name select bug

--- data/player_names_list.asm
+++ data/player_names_list.asm
@@ -1,11 +1,11 @@
 DefaultNamesPlayerList:
 	db "NEW NAME@"
-	db "YELLOW@"
 	db "ASH@"
-	db "JACK@"
+	db "BANNER@"
+	db "NOAH@"
 
 DefaultNamesRivalList:
 	db "NEW NAME@"
 	db "BLUE@"
 	db "GARY@"
-	db "JOHN@"
+	db "SONY@"
